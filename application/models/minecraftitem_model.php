a59b1777723a6e34ee43ffb3da53bfccd82bbaf1
Jasper Wiegratz <jwhy@jwhy.de>
2013-08-28 15:45:04 +0200

    Create basic Minecraft Item model based on CRUD base model
    Signed-off-by: JWhy <jwhy@jwhy.de>

---
+++ application/models/minecraftitem_model.php
@@ -0,0 +1,16 @@
+<?php
+
+class Minecraftitem_model extends My_Model {
+    protected $return_type = 'array';
+    
+    public function apply_update($items){
+      $updated = 0;
+      foreach($items as $item){
+        
+      }
+      return($updated);
+    }
+    
+}
+
+?>
