2f2b94f986662a2422f3fef40999b9b57a9e51be
Jasper Wiegratz <jwhy@jwhy.de>
2013-08-28 15:31:53 +0200

    Extend configs (autoload database, ItemAPI URL)
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/config/craftshop.php
+++ application/config/craftshop.php
@@ -1,9 +1,21 @@
 <?php
 
 $config['jsonapi'] = array(
+    
+    // JSONAPI connection settings
     'host' => 'localhost',
     'port' => '20059',
     'username' => 'craftshop',
     'password' => '',
     'salt' => ''
+)
+;
+
+$config['itemapi'] = array(
+    
+    'urls' => array(
+        'image' => 'http://static.adminiumapp.com/data_values/images/%1$u.png',
+        'list' => 'http://static.adminiumapp.com/data_values/cache.json'
+    )
+    
 );
