fcd27d7d277cd773b4a431afda5ff066c92f57f1
Jasper Wiegratz <jwhy@jwhy.de>
2013-08-31 20:47:04 +0200

    Finish implementing apply_update() method in MinecraftItem model
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/models/minecraftitem_model.php
+++ application/models/minecraftitem_model.php
@@ -5,8 +5,20 @@ class Minecraftitem_model extends My_Model {
     
     public function apply_update($items){
       $updated = 0;
+      
       foreach($items as $item){
+        $data = array(
+        	'name' => $item->name,
+            'item_id' => $item->id,
+            'item_damage' => $item->data_value 
+        );
         
+        $where = $data;
+        unset($where['name']);
+        if(sizeof($this->get_by($where)) == 0){
+          $this->insert($data);
+          $updated++;
+        }
       }
       return($updated);
     }
