928b6a0b2ede9abf5a36157988b972a7032e0998
Jasper Wiegratz <jwhy@jwhy.de>
2013-09-01 15:33:40 +0200

    Added counting of skipped elements in minecraftitem model
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/models/minecraftitem_model.php
+++ application/models/minecraftitem_model.php
@@ -3,24 +3,41 @@
 class Minecraftitem_model extends My_Model {
     protected $return_type = 'array';
     
+    private $default_inavailable = array('0-0', '383-0');
+    
     public function apply_update($items){
-      $updated = 0;
+      $status['updated'] = 0;
+      $status['skipped'] = 0;
       
       foreach($items as $item){
-        $data = array(
-        	'name' => $item->name,
-            'item_id' => $item->id,
-            'item_damage' => $item->data_value 
-        );
-        
-        $where = $data;
-        unset($where['name']);
-        if(sizeof($this->get_by($where)) == 0){
-          $this->insert($data);
-          $updated++;
+        ($this->update_item($item)) ? $status['updated']++ : $status['skipped']++;
+        foreach($item->data_values as $subitem){
+          ($this->update_item($subitem)) ? $status['updated']++ : $status['skipped']++;
         }
       }
-      return($updated);
+      return($status);
+    }
+    
+    private function update_item($item){
+      $data = array(
+          'name' => $item->name,
+          'item_id' => $item->id,
+          'item_damage' => $item->data_value
+      );
+      
+      $where = $data;
+      unset($where['name']);
+      
+      $match = $this->get_by($where);
+      
+      if(sizeof($match) == 0){
+        if(in_array($data['item_id'] . '-' .  $data['item_damage'], $this->default_inavailable)){
+          $data['available'] = 0;
+        }
+        $this->insert($data);
+        return(true);
+      }
+      return(false);
     }
     
 }
