1cda4a7964b59c0bd2eb077159d306485f6256b6
Jasper Wiegratz <jwhy@jwhy.de>
2013-09-22 18:48:48 +0200

    Display item offer table in item detail page view
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/views/items/view_item.php
+++ application/views/items/view_item.php
@@ -3,8 +3,21 @@
 <p>Data value: <?php echo($item->item_damage); ?></p>
 <br>
 <p>Available offers:</p>
-<?php if(sizeof($offers) == 0): ?><div class="alert alert-danger">
+<?php if(sizeof($item->offers) == 0): ?><div class="alert alert-danger">
     There are currently no offers for this item.
-</div><?php else:?>
-
+</div><?php else: ?> 
+  <table class="viewitem_offers table ">
+    <tr>
+      <th>Quantity</th>
+      <th>Price</th>
+      <th></th>
+    </tr>
+    <?php foreach($item->offers as $offer): ?> 
+    <tr>
+      <td><?php echo $offer->quantity; ?></td>
+      <td><?php echo $offer->price . $currency; ?></td>
+      <td><?php echo anchor('items/buy/' . $item->id, 'Buy', array('class' => 'btn btn-mini btn-success')); ?></td>
+    </tr>
+    <?php endforeach; ?> 
+  </table>
 <?php endif;?>
