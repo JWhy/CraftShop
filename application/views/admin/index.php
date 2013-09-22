211c21f1e98cc9b3e5ee07c393e7bd141d6abe8e
Jasper Wiegratz <jwhy@jwhy.de>
2013-09-22 13:35:19 +0200

    Link to new item page location in admin panel view
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,5 +1,5 @@
 <div class="span2">
-  <div class="admin_button_cont"><?php echo anchor('admin/items', 'Edit Item Database', array('class' => 'btn btn-primary')) ?></div>
+  <div class="admin_button_cont"><?php echo anchor('items', 'Edit Item Database', array('class' => 'btn btn-primary')) ?></div>
   <div class="admin_button_cont"><?php echo anchor('admin/home/update_items', 'Update Items', array('class' => 'btn btn-primary')) ?></div>
   <div class="admin_button_cont"><?php echo anchor('auth/index', 'Edit Users', array('class' => 'btn btn-primary')) ?></div>
 </div>
